161dd48f88b00b6eac9326098f3f5dde2874e4a7
CrisVOJ <cristopheroliverajimenez@gmail.com>
2025-08-03 17:28:07 -0400

    feat(backend): backend was added

---
+++ backend-simsml/src/main/java/bo/edu/ucb/backend_simsml/BackendSimsmlApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.backend_simsml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSimsmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSimsmlApplication.class, args);
+	}
+
+}
---
+++ backend-simsml/src/test/java/bo/edu/ucb/backend_simsml/BackendSimsmlApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.backend_simsml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSimsmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
