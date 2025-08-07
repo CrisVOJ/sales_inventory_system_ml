ead4720b8ba4cc2964780bce80bc2d329dfa9ef5
CrisVOJ <cristopheroliverajimenez@gmail.com>
2025-08-06 22:56:35 -0400

    feat(security): security config was added

---
+++ backend-simsml/src/main/java/bo/edu/ucb/backend_simsml/service/UserDetailServiceImpl.java
@@ -0,0 +1,46 @@
+package bo.edu.ucb.backend_simsml.service;
+
+import bo.edu.ucb.backend_simsml.entity.UserEntity;
+import bo.edu.ucb.backend_simsml.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userEntity = userRepository.findUserEntityByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("El usuario " + username + " no fue encontrado"));
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        userEntity.getRoles()
+                .forEach(role -> authorities.add(new SimpleGrantedAuthority("ROLE_".concat(role.getName()))));
+
+        userEntity.getRoles().stream()
+                .flatMap(role -> role.getPermissions().stream())
+                .forEach(permission -> authorities.add(new SimpleGrantedAuthority(permission.getName())));
+
+        return new User(userEntity.getUsername(),
+                userEntity.getPassword(),
+                userEntity.isEnabled(),
+                userEntity.isAccountNoExpired(),
+                userEntity.isCredentialNoExpired(),
+                userEntity.isAccountNoLocked(),
+                authorities);
+    }
+
+}
