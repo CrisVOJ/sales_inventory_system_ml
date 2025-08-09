67d3a44c78ef263db7967592d2f3ce12697ff21a
CrisVOJ <cristopheroliverajimenez@gmail.com>
2025-08-08 21:02:17 -0400

    feat(globals): globals file was added

---
+++ backend-simsml/src/main/java/bo/edu/ucb/backend_simsml/config/util/Globals.java
@@ -0,0 +1,8 @@
+package bo.edu.ucb.backend_simsml.config.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Globals {
+    public static final String baseApi = "api/v1/";
+}
